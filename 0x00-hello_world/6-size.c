32b1fe05cb65071cc8de641d30c274034e076a8d
rahman-a <ahm.abdrahman@gmail.com>
2023-07-13 19:34:02 -0700

    fix the typo of sizeOf to sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n", sizeOf(char));
-	printf("Size of an int: %ld byte(s)\n", sizeOf(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeOf(long int));
-	printf("Size of a long long int: %ld byte(s)\n", sizeOf(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeOf(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
