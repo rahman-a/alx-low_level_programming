17e1969d24250c2cb568c31866ada3397966eb4b
rahman-a <ahm.abdrahman@gmail.com>
2023-07-13 18:55:08 -0700

    add double quote before sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description - C Program that prints using puts function
+ * Description:  C Program that prints using puts function
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
