c2f14c624191545763a50ae2079be068d72721a7
rahman-a <ahm.abdrahman@gmail.com>
2023-07-13 19:58:37 -0700

    modify the style of 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quote, 59);
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quot, 59);
+
 	return (1);
 }
