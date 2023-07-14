a5c56b4edc53439785becf3af52dc9c64799c9e6
rahman-a <ahm.abdrahman@gmail.com>
2023-07-14 02:06:31 -0700

    Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints the alphabet in lowercase
+ * and then in uppercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char lower_case = 'a';
+	char upper_case = 'A';
+
+	/* print lower case alphabet */
+	while (lower_case < 'z')
+	{
+		putchar(lower_case);
+		lower_case++;
+	};
+
+	/* print upper case alphabet */
+	while (upper_case < 'Z')
+	{
+		putchar(upper_case);
+		upper_case++;
+	};
+
+	putchar('\n');
+	return (0);
+}
