ea88a82472f3a9d8d2d5ac30c630db142ca3606e
rahman-a <ahm.abdrahman@gmail.com>
2023-07-19 11:47:01 -0700

    add semicolon after printf function in file 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,9 +15,9 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
 	};
-	printf("%d\n", sum)
+	printf("%d\n", sum);
 	return (0);
 }
