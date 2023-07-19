b7284068f95874bef579f3ce2266eeae0aa8d7d1
rahman-a <ahm.abdrahman@gmail.com>
2023-07-19 15:00:14 -0700

    use while loop and make condition sum <= four million

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,10 +11,9 @@
 
 int main(void)
 {
-	long int i;
 	long int sq1 = 1, sq2 = 2, sum = 0, evenNumbers = 0;
 
-	for (i = 0; i < 4000000; i++)
+	while (sum <= 4000000)
 	{
 		sum = sq1 + sq2;
 		sq1 = sq2;
@@ -23,6 +22,6 @@ int main(void)
 		if (sum % 2 == 0)
 			evenNumbers += sum;
 	};
-	printf("%ld\n", evenNumbers);
+	printf("%lu\n", evenNumbers);
 	return (0);
 }
