2d136828f17d920b95201d33eba6a33e9eed9382
rahman-a <ahm.abdrahman@gmail.com>
2023-07-19 16:44:56 -0700

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#define MAX 100000000
+
+/**
+ * numLength - a function to calculate the length of number
+ * @n: the targeted number to get its length
+ *
+ * Return: numerical rep fo the number length
+*/
+
+int numLength(int n)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	};
+	return (length);
+}
 
 /**
  * main - Entry point
@@ -11,19 +33,30 @@
 
 int main(void)
 {
-	int count = 0;
-	unsigned long sq1 = 0, sq2 = 1, sum = 0;
+	int count = 0, zeros;
+	unsigned long sq1 = 0, sq2 = 1, sum = 0, sq1a = 0, sq2a = 0, suma = 0;
 
 	while (count < 98)
 	{
-		sum = sq1 + sq2;
+		if (sq1a > 0)
+			printf("%lu", sq1a);
+		zeros = numLength(MAX) - 1 - numLength(sq1);
+		while (sq1a > 0 && zeros > 0)
+		{
+			printf("%d", 0);
+			zeros--;
+		}
+		printf("%lu", sq1);
+		sum = (sq1 + sq2) % MAX;
+		suma = sq1a + sq2a + (sq1 + sq2) / MAX;
 		sq1 = sq2;
 		sq2 = sum;
-
+		sq1a = sq2a;
+		sq2a = suma;
 		if (count == 97)
-			printf("%lu\n", sum);
+			printf("\n");
 		else
-			printf("%lu, ", sum);
+			printf(", ");
 		count++;
 	};
 	return (0);
