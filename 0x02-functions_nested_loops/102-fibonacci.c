f3e88d41c138e66a572f559a73774c0844c6ff33
rahman-a <ahm.abdrahman@gmail.com>
2023-07-19 12:47:51 -0700

    make the 48 loop not 50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,9 +17,9 @@ int main(void)
 
 	printf("%lu, %lu, ", sq1, sq2);
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 48; i++)
 	{
-		if (i == 49)
+		if (i == 47)
 			printf("%lu \n", sum);
 		else
 			printf("%lu, ", sum);
