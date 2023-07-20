f7c25051a1d34a81f36ffadad908d6fc40cbed11
rahman-a <ahm.abdrahman@gmail.com>
2023-07-20 07:51:18 -0700

    use for statement instead of while in 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,13 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
-
 		_putchar(i + '0');
-		i++;
 	};
-
 	_putchar('\n');
 }
