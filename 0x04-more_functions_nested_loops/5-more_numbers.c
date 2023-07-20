30d8af8c5f11faf5b2d76bac7e35e4354f571182
rahman-a <ahm.abdrahman@gmail.com>
2023-07-20 08:10:24 -0700

    split number larger than 10 into two digit print 1 with putchar then print last digit with putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * Return: (void)
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			int last_number = j % 10;
+
+			if (j > 9)
+				_putchar(1 + '0');
+			_putchar(last_number + '0');
+		};
+		_putchar('\n');
+	};
+}
