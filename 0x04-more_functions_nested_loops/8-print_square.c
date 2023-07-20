939faf25fc7f55e48def1b865b35eb02f4d849fa
rahman-a <ahm.abdrahman@gmail.com>
2023-07-20 10:29:45 -0700

    add conditional statement if size is less or equal to 0 print only new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,11 @@ void print_square(int size)
 {
 	int row, column;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	};
 	for (row = 0; row < size; row++)
 	{
 		for (column = 0; column < size; column++)
