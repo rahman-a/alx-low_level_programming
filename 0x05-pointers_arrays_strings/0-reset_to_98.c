775a3532b6960ec1798d2fba71c826c4da83e27d
rahman-a <ahm.abdrahman@gmail.com>
2023-07-24 11:16:38 -0700

    add description for paramenter in in file 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @*n:integer  pointer
+ * @n: integer pointer
  * Return: (void)
  */
 
