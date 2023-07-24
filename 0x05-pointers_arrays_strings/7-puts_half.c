a0e8fb07183aa2aded2eac1b83e48dc4c106a314
rahman-a <ahm.abdrahman@gmail.com>
2023-07-24 13:23:19 -0700

    Write a function that prints every other character of a string, starting with the first character, followed by a new line. Write a function that prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string, followed by a new line
+ * @str: string
+ * Return:(void)
+*/
+
+void puts_half(char *str)
+{
+	int len;
+
+	while (str[l])
+		len++;
+
+	for (len /= 2; str[len] != '\0'; len++)
+		_putchar(str[len]);
+	_putchar('\n');
+}
