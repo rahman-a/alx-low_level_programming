c599fbd43c4a2d5ce8a79a08f5423f065be8fb4b
rahman-a <ahm.abdrahman@gmail.com>
2023-07-24 13:07:19 -0700

    a

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts - a function that prints a string, followed by a new line, to stdout.
- * @*str: string to print
+ * @str: string to print
  * Return: (void)
 */
 
@@ -10,7 +10,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str + '0');
+		_putchar(*str + 0);
 		str++;
 	};
 
