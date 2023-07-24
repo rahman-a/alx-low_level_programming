9697f79a4e6fa2da52cee076477e44078f970d01
rahman-a <ahm.abdrahman@gmail.com>
2023-07-24 13:31:32 -0700

    initiate variable len with 0 in file 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,11 @@
 
 void puts_half(char *str)
 {
-	int len;
+	int len = 0;
 
 	while (str[len])
 		len++;
-	len++;
+	len++
 	for (len /= 2; str[len] != '\0'; len++)
 		_putchar(str[len]);
 	_putchar('\n');
