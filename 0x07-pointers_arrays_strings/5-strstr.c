1f1891c94bb37d413874bb7fd4dfd099e9bcbce0
rahman-a <ahm.abdrahman@gmail.com>
2023-07-31 05:58:51 -0700

    create function to print second dimension array values in rows

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strstr - function finds the first occurrence of the substring needle
+ * in the string haystack. The terminating null bytes (\0) are not compared
+ * @haystack: string
+ * @needle: string
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
