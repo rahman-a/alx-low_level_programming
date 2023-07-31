10a3e70921c8864bf19082fb7ac6c08b7574aff8
rahman-a <ahm.abdrahman@gmail.com>
2023-07-31 05:21:22 -0700

    change condition in while loop in file 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,10 +13,10 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0, j;
 
-	while (s[i] >= '\0')
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j] >= '\0'; j++)
-			if (s[i] == accept[j])
+		for (j = 0; accept[j] != s[i]; j++)
+			if (accept[j] == '\0')
 				return (i);
 		i++;
 	}
