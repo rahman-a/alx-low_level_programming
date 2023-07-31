48533bd92feb7e2dd509062982d2e6178869bdf6
rahman-a <ahm.abdrahman@gmail.com>
2023-07-31 06:39:50 -0700

    add standard library in file 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strstr - function finds the first occurrence of the substring needle
  * in the string haystack. The terminating null bytes (\0) are not compared
