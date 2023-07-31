de27cc36562f40c4546f389c186f54f760c3eb73
rahman-a <ahm.abdrahman@gmail.com>
2023-07-31 06:14:33 -0700

    a function to add diagonal elements of the 2d array to each other

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - a function to add diagonal number of 2d array to eahc other
+ * @a: 2d array
+ * @size: size of array
+ * Return: (void)
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, acc1, acc2;
+
+	for (i = 0; i < size; i++)
+	{
+		acc1 = a[i];
+		acc2 = a[size - i - 1];
+		a += size;
+	}
+	printf("%d,  ", acc1);
+	printf("%d\n", acc2);
+}
+
