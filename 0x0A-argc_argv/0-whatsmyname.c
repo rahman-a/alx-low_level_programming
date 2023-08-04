87e69eaa35397376b810230fd53e96983e3843ee
rahman-a <ahm.abdrahman@gmail.com>
2023-08-04 09:30:11 -0700

    add description for function paramters in file 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,8 @@
 /**
  * main - Entry point
  * Description: a program that prints its name, followed by a new line.
+ * @argc: arguments count
+ * @argv: arguments values
  * Return: Always 0 (Success)
 */
 
