86da99359ec91c7e1ae3967371be062e4ef38ed3
rahman-a <ahm.abdrahman@gmail.com>
2023-08-10 15:29:50 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,11 +29,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 		i++;
 	}
-	if (n >= s2Len)
-	{
-		str[i] = *s2;
-		return (str);
-	}
 	while (j < n)
 	{
 		str[i] = s2[j];
