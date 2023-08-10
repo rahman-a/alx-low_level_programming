4a1007b75241f77123969d78fa9eea34baf55ea7
rahman-a <ahm.abdrahman@gmail.com>
2023-08-10 16:18:18 -0700

    fi

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -38,7 +38,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	loc = malloc(sizeOfArr);
-	if (loc == NULL)
+	if (loc == 0)
 		return (NULL);
 	_memset(loc, 0, sizeOfArr);
 	return (loc);
