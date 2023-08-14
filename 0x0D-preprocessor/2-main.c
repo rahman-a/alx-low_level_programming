b0bc0a53694f23a10d5515306b95e44daea8ff84
rahman-a <ahm.abdrahman@gmail.com>
2023-08-14 14:05:30 -0700

    Create a header file that defines a macro named SIZE as an abbreviation for the token 1024. Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359. Write a program that prints the name of the file it was compiled from, followed by a new line Write a function-like macro ABS(x) that computes the absolute value of a number x. Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the current file
+ *
+ * Return: Always success (0)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_H
+#define _SUM.H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
