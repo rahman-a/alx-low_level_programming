4fa1d65b95ff29d8b89b79ea84248f20c3a02a39
rahman-a <ahm.abdrahman@gmail.com>
2023-08-14 12:41:28 -0700

    free malloc for dog->name if dog->owner is null in file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,6 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(_strlen(owner) + 1);
 	if ((*dog).owner == NULL)
 	{
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
