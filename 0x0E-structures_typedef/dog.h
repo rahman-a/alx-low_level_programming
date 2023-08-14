4f257a944610e79c909cba228c21418cd8e620f0
rahman-a <ahm.abdrahman@gmail.com>
2023-08-14 09:09:43 -0700

    adjust the betty styling for the file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-struct dog {
-	char* name;
+
+/**
+ * struct dog - a struct for dog
+ * @name: character for dog name
+ * @age: float fot a dog age
+ * @owner: character for dog owner
+ * Description: a structre collecting a data about Dog
+*/
+
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif
